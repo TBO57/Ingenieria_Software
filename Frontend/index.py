2c1e34e6418446f954aa3c833db63870283e73c6
TBO57 <emadridr05@gmai.com>
2022-07-22 00:46:02 -0500

    Carpeta Frontend

---
+++ Frontend/index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/operador')
+def operador():
+    return render_template('operador.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
