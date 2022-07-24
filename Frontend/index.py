4588d0486c12fa9fbf49591db2bd6911c581e0e9
TBO57 <emadridr05@gmai.com>
2022-07-23 20:25:47 -0500

    commit_message = input("Commit message: commit rutas")

--- Frontend/index.py
+++ Frontend/index.py
@@ -18,35 +18,54 @@ db = MySQL(app)
 def index():
     return render_template('index.html')
 
+# ///////////////////////////////////////rutas admin/////////////////////////////
 
-@app.route('/login_admin')
+
+@app.route('/admin/login')
 def login_admin():
-    return render_template('login_admin.html')
+    return render_template('/admin/login_admin.html')
 
 
-@app.route('/login_operador', methods=['GET', 'POST'])
+@app.route('/admin/inicio')
+def inicio_admin():
+    return render_template('/admin/inicioAdmin.html')
+
+
+@app.route('/admin/registro_operador', methods=['GET', 'POST'])
+def registro_operador():
+    return render_template('registro_operador.html')
+
+
+@app.route('/admin/reporte')
+def reporte_admin():
+    return render_template('/admin/reporte_ventas.html')
+
+# ///////////////////////////////////////rutas admin/////////////////////////////
+
+
+# ///////////////////////////////////////rutas operador/////////////////////////////
+@app.route('/operador/login', methods=['GET', 'POST'])
 def login_operador():
     if request.method == 'POST':
         print(request.form['username'])
         print(request.form['password'])
-        return render_template('login_operador.html')
+        return redirect('/operador/inicio')
     else:
-        return render_template('login_operador.html')
+        return render_template('/op/login_operador.html')
 
 
-@app.route('/inicio_operador')
+@app.route('/operador/inicio')
 def inicio_operador():
-    return render_template('inicio_operador.html')
+    return render_template('/op/inicioOperador.html')
 
 
-@app.route('/inicio_admin')
-def inicio_admin():
-    return render_template('inicio_admin.html')
+# ///////////////////////////////////////rutas operador/////////////////////////////
 
 
-@app.route('/registro_operador', methods=['GET', 'POST'])
-def registro_operador():
-    return render_template('registro_operador.html')
+# ///////////////////////////////////////rutas comunes/////////////////////////////
+@app.route('/recarga')
+def interfaz_recarga():
+    return render_template('recarga.html')
 
 
 @app.route('/registro_estudiante')
@@ -57,27 +76,12 @@ def registro_estudiante():
 @app.route('/actualizar_estudiante')
 def actualizar_estudiante():
     return render_template('actualizar_estudiante.html')
+# ///////////////////////////////////////rutas comunes/////////////////////////////
 
-
-@app.route('/recarga')
-def recarga():
-    return render_template('recarga.html')
-
-@app.route('/dashboard/operador')
-def dashboard_operador():
-    return render_template('inicioOperador.html')
-
-@app.route('/dashboard/admin')
-def dashboard_admin():
-    return render_template('inicioAdmin.html')
 
 @app.route('/operador/reporte')
 def interfaz_reporte():
     return render_template('reporteFallos.html')
-
-@app.route('/operador/recarga')
-def interfaz_recarga():
-    return render_template('recarga.html')
 
 
 if __name__ == '__main__':
