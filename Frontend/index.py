84ea13e290ece8751f8df93d4cf50ce5e5c9a6c0
Santiago Ruiz <sanandres2504@gmail.com>
2022-07-24 13:30:30 -0500

    .

--- Frontend/index.py
+++ Frontend/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL, MySQLdb
-
+import requests
 # pip install Flask Flask-MySQLdb Flask_wtf Flask_login
 
 
@@ -141,12 +141,74 @@ def interfaz_reporte():
 # @app.route('/admin/inicio')
 # def inicio_admin():
 #     return render_template('/admin/inicioAdmin.html')
-
+#
 
 # ///////////////////////////////////////rutas comunes/////////////////////////////
 
 @app.route('/recarga')
 def interfaz_recarga():
+
+    if request.method == 'POST':
+        ndocumento = request.form['Cuenta']
+        # password = request.form['password']
+
+        print(ndocumento)
+        # print(password)
+
+        cur = db.connection.cursor()
+        cur.execute("SELECT * FROM persona WHERE N_Documento='{0}'".format(ndocumento))
+        user = cur.fetchone()
+        cur.close()
+
+
+        if user != None:
+            # if password == user[3]:
+
+                idb = user[1];
+                print(idb)
+
+                # query = db.connection.cursor()
+                # query.execute("SELECT * FROM persona WHERE Id='{0}'".format(idb))
+                # person = query.fetchone()
+                # query.close()
+
+                # session['name'] = person[3]
+                # session['email'] = person[7]
+
+                url = 'http://127.0.0.1:5000/recarga'
+
+                datos= {
+                    'Ndocumento' : user[1],
+                    'Nombre' : user[3],
+                    'Apellido' : user[5]
+                }
+
+                headers = {
+                    'User-Agent' : 'Mozilla/5.0',
+                    'Agent-Version' : '1.0.0'
+
+                }
+
+                requests.post(url, data=datos, headers=headers)
+
+                return render_template('recarga.html')
+
+            # else:
+
+            #     print("Usuario o contraseña inválido")
+            #     return render_template("/op/login_operador.html")
+
+        else:
+
+            print("Este Usuario no está registrado")
+            
+            return render_template("/op/login_operador.html")
+    else:
+        
+        return render_template('recarga.html')
+
+
+
     return render_template('recarga.html')
 
 
