c7b111135e7c834287f27083714fa56a90fe9ead
Santiago Ruiz <sanandres2504@gmail.com>
2022-07-25 14:03:58 -0500

    Cambio de rutas comunes

--- Frontend/index.py
+++ Frontend/index.py
@@ -109,6 +109,137 @@ def registro_operador():
         return redirect('/admin/registro_operador')
 
 
+@app.route('/admin/registro_estudiante', methods=['GET', 'POST'])
+def adm_registro_estudiante():
+
+    if request.method == 'GET':
+        return render_template("/admin/registro_estudiante.html")
+
+    else:
+
+        tipo = request.form['tipodocumento']
+        nom1 = request.form['primernombre']
+        numdoc = request.form['ndocumento']
+        nom2 = request.form['segundonombre']
+        ape1 = request.form['primerapellido']
+        ape2 = request.form['segundoapellido']
+        numtel = request.form['ntelefono']
+        email = request.form['email']
+
+        cur = db.connection.cursor()
+        cur.execute("INSERT INTO persona (N_Documento, Tipo_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Correo, Celular) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+                    (numdoc, tipo, nom1, nom2, ape1, ape2, email, numtel))
+        db.connection.commit()
+
+        cur = db.connection.cursor()
+        cur.execute(
+            "INSERT INTO estudiante (Persona_ID) VALUES ((SELECT MAX(Id) FROM persona))")
+        db.connection.commit()
+
+        flash("El registro del estudiante ha sido exitoso")
+        return redirect('/admin/inicio')
+
+
+@app.route('/admin/recarga', methods=['GET', 'POST'])
+def recarga_admin():
+
+    if request.method == 'POST':
+        ndocumento = request.form['cuenta']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        valor = request.form['valor']
+
+        cur = db.connection.cursor()
+        cur.execute(
+            "SELECT * FROM persona WHERE N_Documento='{0}'".format(ndocumento))
+        user = cur.fetchone()
+        cur.close()
+
+
+        if user != None:
+
+            userdoc = str(user[1])
+            if userdoc == ndocumento and user[3] == nombre and user[5] == apellido:
+
+                pid = user[0]
+                ndoc = user[1]
+                nom = user[3]
+                ape = user[5]
+
+                query1 = db.connection.cursor()
+                query1.execute(
+                    "SELECT * FROM estudiante WHERE Persona_Id='{0}'".format(pid))
+                est = query1.fetchone()
+                query1.close()
+
+                idest = est[0]
+
+                query2 = db.connection.cursor()
+                query2.execute(
+                    "SELECT * FROM cuenta WHERE Estudiante_idEstudiante='{0}'".format(idest))
+                acc = query2.fetchone()
+                query2.close()
+
+                saldo = int(acc[2])
+
+                valortotal = int(valor) + saldo
+
+                if valortotal < 50:
+
+                    insert = db.connection.cursor()
+                    insert.execute(
+                        "UPDATE cuenta SET Saldo=%s WHERE Estudiante_idEstudiante=%s", (valortotal, idest))
+                    db.connection.commit()
+
+                    flash("Recarga realizada con éxito")
+                    return redirect('/admin/recarga')
+
+                else:
+
+                    flash("Esta recarga sobrepasa el limite de almuerzos")
+                    return redirect('/admin/recarga')
+
+            else:
+                flash("Los datos no coinciden")
+                print("Los campos no coinciden")
+                return redirect("/admin/recarga")
+
+        else:
+            flash("El Usuario no está registrado")
+            print("Este Usuario no está registrado")
+
+            return render_template('/admin/recarga.html')
+    else:
+
+        return render_template('/admin/recarga.html')
+
+
+@app.route('/admin/actualizar_estudiante', methods=['GET', 'POST'])
+def adm_actualizar_estudiante():
+
+    if request.method == 'GET':
+        return render_template('/admin/actualizar_estudiante.html')
+
+    else:
+
+        tipo = request.form['tipodocumento']
+        numdoc = request.form['ndocumento']
+        nom1 = request.form['primernombre']
+        nom2 = request.form['segundonombre']
+        ape1 = request.form['primerapellido']
+        ape2 = request.form['segundoapellido']
+        numtel = request.form['ntelefono']
+        email = request.form['email']
+
+        cur = db.connection.cursor()
+        cur.execute("UPDATE persona SET N_Documento = %s, Tipo_Documento = %s, Primer_Nombre = %s, Segundo_Nombre   = %s, Primer_Apellido = %s, Segundo_Apellido = %s, Correo = %s, Celular = %s WHERE N_Documento = %s",
+                    (int(numdoc), tipo, nom1, nom2, ape1, ape2, email, int(numtel), int(numdoc)))
+        db.connection.commit()
+
+        flash("Actualización realizada con éxito")
+        return redirect('/admin/actualizar_estudiante')
+
+
 @app.route('/admin/reporte')
 def reporte_admin():
     return render_template('/admin/reporte_ventas.html')
@@ -137,9 +268,6 @@ def login_operador():
     if request.method == 'POST':
         usuario = request.form['username']
         password = request.form['password']
-
-        # print(usuario)
-        # print(password)
 
         cur = db.connection.cursor()
         cur.execute(
@@ -183,29 +311,39 @@ def inicio_operador():
     return render_template('/op/inicioOperador.html')
 
 
-@app.route('/operador/reporte', methods=['GET', 'POST'])
-def interfaz_reporte():
+@app.route('/operador/registro_estudiante', methods=['GET', 'POST'])
+def op_registro_estudiante():
 
-    if request.method == 'POST':
-        body = request.form['fallo']
-
-        cur = db.connection.cursor()
-        cur.execute(
-            "INSERT INTO reporte (descripcion) VALUES ('{0}')".format(body))
-        db.connection.commit()
-
-        flash("Reporte enviado con exito")
-        return redirect('/operador/reporte')
+    if request.method == 'GET':
+        return render_template("/op/registro_estudiante.html")
 
     else:
 
-        return render_template('/op/reporteFallos.html')
+        tipo = request.form['tipodocumento']
+        nom1 = request.form['primernombre']
+        numdoc = request.form['ndocumento']
+        nom2 = request.form['segundonombre']
+        ape1 = request.form['primerapellido']
+        ape2 = request.form['segundoapellido']
+        numtel = request.form['ntelefono']
+        email = request.form['email']
+
+        cur = db.connection.cursor()
+        cur.execute("INSERT INTO persona (N_Documento, Tipo_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Correo, Celular) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+                    (numdoc, tipo, nom1, nom2, ape1, ape2, email, numtel))
+        db.connection.commit()
+
+        cur = db.connection.cursor()
+        cur.execute(
+            "INSERT INTO estudiante (Persona_ID) VALUES ((SELECT MAX(Id) FROM persona))")
+        db.connection.commit()
+
+        flash("El registro del estudiante ha sido exitoso")
+        return redirect('/operador/inicio')
 
 
-# ///////////////////////////////////////rutas comunes/////////////////////////////
-
-@app.route('/recarga', methods=['GET', 'POST'])
-def interfaz_recarga():
+@app.route('/operador/recarga', methods=['GET', 'POST'])
+def recarga_operador():
 
     if request.method == 'POST':
         ndocumento = request.form['cuenta']
@@ -219,9 +357,10 @@ def interfaz_recarga():
         user = cur.fetchone()
         cur.close()
 
-        userdoc = str(user[1])
-
+        
         if user != None:
+
+            userdoc = str(user[1])
             if userdoc == ndocumento and user[3] == nombre and user[5] == apellido:
 
                 pid = user[0]
@@ -255,27 +394,74 @@ def interfaz_recarga():
                     db.connection.commit()
 
                     flash("Recarga realizada con éxito")
-                    return redirect('/recarga')
+                    return redirect('/operador/recarga')
 
                 else:
 
                     flash("Esta recarga sobrepasa el limite de almuerzos")
-                    return redirect('/recarga')
+                    return redirect('/operador/recarga')
 
             else:
                 flash("Los datos no coinciden")
                 print("Los campos no coinciden")
-                return redirect("/recarga")
+                return redirect("/operador/recarga")
 
         else:
             flash("El Usuario no está registrado")
-            print("Este Usuario no está registrado")
-
-            return render_template("recarga.html")
+            return render_template('/op/recarga.html')
     else:
 
-        return render_template('recarga.html')
+        return render_template('/op/recarga.html')
 
+
+@app.route('/operador/actualizar_estudiante', methods=['GET', 'POST'])
+def op_actualizar_estudiante():
+
+    if request.method == 'GET':
+        return render_template('/op/actualizar_estudiante.html')
+
+    else:
+
+        tipo = request.form['tipodocumento']
+        numdoc = request.form['ndocumento']
+        nom1 = request.form['primernombre']
+        nom2 = request.form['segundonombre']
+        ape1 = request.form['primerapellido']
+        ape2 = request.form['segundoapellido']
+        numtel = request.form['ntelefono']
+        email = request.form['email']
+
+        cur = db.connection.cursor()
+        cur.execute("UPDATE persona SET N_Documento = %s, Tipo_Documento = %s, Primer_Nombre = %s, Segundo_Nombre   = %s, Primer_Apellido = %s, Segundo_Apellido = %s, Correo = %s, Celular = %s WHERE N_Documento = %s",
+                    (int(numdoc), tipo, nom1, nom2, ape1, ape2, email, int(numtel), int(numdoc)))
+        db.connection.commit()
+
+        print("funcionando")
+
+        flash("Actualización realizada con éxito")
+        return redirect('/operador/actualizar_estudiante')
+
+
+@app.route('/operador/reporte', methods=['GET', 'POST'])
+def interfaz_reporte():
+
+    if request.method == 'POST':
+        body = request.form['fallo']
+
+        cur = db.connection.cursor()
+        cur.execute(
+            "INSERT INTO reporte (descripcion) VALUES ('{0}')".format(body))
+        db.connection.commit()
+
+        flash("Reporte enviado con exito")
+        return redirect('/operador/reporte')
+
+    else:
+
+        return render_template('/op/reporteFallos.html')
+
+
+# ///////////////////////////////////////rutas comunes/////////////////////////////
 
 @app.route('/registro_estudiante', methods=['GET', 'POST'])
 def registro_estudiante():
@@ -307,33 +493,6 @@ def registro_estudiante():
         flash("El registro del estudiante ha sido exitoso")
         return redirect('/registro_estudiante')
 
-
-@app.route('/actualizar_estudiante', methods=['GET', 'POST'])
-def actualizar_estudiante():
-
-    if request.method == 'GET':
-        return render_template('actualizar_estudiante.html')
-
-    else:
-
-        tipo = request.form['tipodocumento']
-        numdoc = request.form['ndocumento']
-        nom1 = request.form['primernombre']
-        nom2 = request.form['segundonombre']
-        ape1 = request.form['primerapellido']
-        ape2 = request.form['segundoapellido']
-        numtel = request.form['ntelefono']
-        email = request.form['email']
-
-        cur = db.connection.cursor()
-        cur.execute("UPDATE persona SET N_Documento = %s, Tipo_Documento = %s, Primer_Nombre = %s, Segundo_Nombre   = %s, Primer_Apellido = %s, Segundo_Apellido = %s, Correo = %s, Celular = %s WHERE N_Documento = %s",
-                    (int(numdoc), tipo, nom1, nom2, ape1, ape2, email, int(numtel), int(numdoc)))
-        db.connection.commit()
-
-        print("funcionando")
-
-        flash("Actualizado realizada con éxito")
-        return redirect('/actualizar_estudiante')
 
 
 # ///////////////////////////////////////rutas comunes/////////////////////////////
